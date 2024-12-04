d01bb015b9f6a72b65a17605f7f38703418e4493
satyam shivam <satyamdk6927@gmail.com>
2024-12-04 14:45:09 +0530

    .

--- java_basics/advance java/InetAddressTest.java
+++ java_basics/advance java/InetAddressTest.java
@@ -1,16 +1,16 @@
 import java.net.*;
+
 public class InetAddressTest {
- public static void main(String args[]) throws UnknownHostException
-{
- InetAddress Address = InetAddress.getLocalHost();
- System.out.println("Local Host : "+Address);
- 
- Address = InetAddress.getByName("www.facebook.com");
- System.out.println("Google Host : "+Address);
- 
- InetAddress sw[] = InetAddress.getAllByName("www.google.com");
- for(int i=0;i<sw.length;i++)
- System.out.println("Google Host : "+sw[i]);
- 
- }
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getLocalHost();
+        System.out.println("Local Host: " + address);
+
+        address = InetAddress.getByName("www.facebook.com");
+        System.out.println("Facebook Host: " + address);
+
+        InetAddress[] sw = InetAddress.getAllByName("www.google.com");
+        for (InetAddress inetAddress : sw) {
+            System.out.println("Google Host: " + inetAddress);
+        }
+    }
 }
